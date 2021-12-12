e5e9bfc2a9aa8ce81976784f7024d61a98c530e6
engrajibulhasan <engrajibulhasan@gmail.com>
2021-12-12 12:29:09 +0600

    External JS style added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
-import './App.css';
+import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
+import "./App.css";
+import { style } from "./Style";
 
 function App() {
+  const dateValue = new Date("02/12/2021 10:30 AM");
+
   return (
     <div className="App">
       <h1>Date time</h1>
-      <div style={{width:"400px",margin:"auto"}}>
-      <DateTimePickerComponent></DateTimePickerComponent>
+      {/* Input */}
+      <div style={{ width: "300px", margin: "auto" }}>
+        <DateTimePickerComponent
+          placeholder="Choose From date"
+          value={dateValue}
+        ></DateTimePickerComponent>
+      </div>
+      <div style={{ width: "300px", margin: "auto" }}>
+        <DateTimePickerComponent placeholder="Choose To date"></DateTimePickerComponent>
+      </div>
+
+      {/* Output */}
+      <div
+        style={style.outputBox}
+      >
+        <h3>Output</h3>
+        <p>From Date:</p>
+        <p>To Date:</p>
+        <p>Total Days:</p>
       </div>
     </div>
   );
---
+++ src/Style.js
@@ -0,0 +1,9 @@
+export const style={
+    outputBox:{
+        width: "300px",
+        margin: "20px auto",
+        textAlign: "Left",
+        border: "4px solid tomato",
+        padding: "0 10px 10px",
+      }
+}
